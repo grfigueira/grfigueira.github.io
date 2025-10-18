4875997d25e0665fd0783c5056370338ec9e5e8b
Guilherme Figueira <g.figueira.2002@gmail.com>
2025-10-18 15:39:03 +0100

    Configure github pages

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   // Copy assets and script.js to the output as-is
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
   eleventyConfig.addPassthroughCopy({ "src/script.js": "script.js" });
+  eleventyConfig.addPassthroughCopy(".nojekyll");
 
   return {
     dir: { input: "src", includes: "_includes", output: "_site" },
