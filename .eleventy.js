1afc1936e59085003860c5d5d83f20c4941cfb2c
Guilherme Figueira <g.figueira.2002@gmail.com>
2025-10-18 15:42:26 +0100

    removed jekyll

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,6 @@ module.exports = function (eleventyConfig) {
   // Copy assets and script.js to the output as-is
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
   eleventyConfig.addPassthroughCopy({ "src/script.js": "script.js" });
-  eleventyConfig.addPassthroughCopy(".nojekyll");
-
   return {
     dir: { input: "src", includes: "_includes", output: "_site" },
     htmlTemplateEngine: "njk"
